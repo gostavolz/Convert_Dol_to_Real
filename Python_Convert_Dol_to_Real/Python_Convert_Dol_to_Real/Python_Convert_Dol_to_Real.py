038943ed4dde1c9a03f4762bfb90354909492761
gostavolz <gustavoluiz13531@gmail.com>
2025-10-07 09:59:38 -0300

    Add files via upload

---
+++ Python_Convert_Dol_to_Real/Python_Convert_Dol_to_Real/Python_Convert_Dol_to_Real.py
@@ -0,0 +1,7 @@
+
+cot = float(input("Qual é a cotação do dólar atual (em reais)? "))
+dol = float(input("Quantos dólares você tem? "))
+
+real = cot * dol
+
+print (f"Voce tem R$ {real:.2f} reais.")
